faf9a4ae51b40403df198c7e0c51a011731b1bd4
flashfinisher7 <mvsanilkumar07@gmail.com>
2021-07-26 14:10:25 +0530

    print head or tail

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
 echo "this is a flip coin simulator program"
+
+function flipCoin()
+{
+	randomCheck=$((RANDOM%2))
+	if [ $randomCheck -eq 0 ]
+	then
+		echo "HEAD"
+	else
+		echo "TAIL"
+	fi
+}
