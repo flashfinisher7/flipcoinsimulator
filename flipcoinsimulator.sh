569e195e07e189b6fa8407ab21331fc5ec0a17d4
flashfinisher7 <mvsanilkumar07@gmail.com>
2021-07-26 14:23:18 +0530

    a simulator, loop

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -11,3 +11,27 @@ function flipCoin()
 		echo "TAIL"
 	fi
 }
+
+
+declare -A singlets
+declare -A  winingCombination
+isHead="HEAD"
+read -p "Enter number of times to flip the coin " n;
+for (( i=0;i<n;i++ ))
+do
+	result="$( flipCoin )"
+	singlets[$i]=$result;
+	if [ "$result" == "$isHead" ]
+	then
+		heads=$((heads+1));
+	else
+		tails=$((tails+1));
+	fi
+done
+echo "Generated Singlets:"
+echo ${singlets[@]};
+echo "Percentage of Singlets";
+echo "Percentage of Heads:= $((heads*100/n))";
+echo "Percentage of Tails:= $((tails*100/n))";
+winingCombination[H]=$((heads*100/n))
+winingCombination[T]=$((tails*100/n))
