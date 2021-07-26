892dbbcf2decf94808e403305b6f6f43c9086705
flashfinisher7 <mvsanilkumar07@gmail.com>
2021-07-26 14:33:48 +0530

    doublet

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -35,3 +35,36 @@ echo "Percentage of Heads:= $((heads*100/n))";
 echo "Percentage of Tails:= $((tails*100/n))";
 winingCombination[H]=$((heads*100/n))
 winingCombination[T]=$((tails*100/n))
+
+declare -A doublets
+hh=0
+ht=0
+th=0
+tt=0
+for (( i=0;i<n;i++ ))
+do
+	result="$( flipCoin )""$( flipCoin )"
+	doublets[$i]=$result;
+	if [ "$result" == "HEADHEAD" ]
+   then
+		hh=$((hh+1));
+	elif [ "$result" == "HEADTAIL" ]
+	then
+		ht=$((ht+1));
+	elif [ "$result" == "TAILHEAD" ]
+	then
+		th=$((th+1));
+	else
+		tt=$((tt+1));
+	fi
+done
+echo "Generated doublets:"
+echo ${doublets[@]};
+echo "Percentage of HH = $((hh*100/n))";
+echo "Percentage of TT = $((tt*100/n))";
+echo "Percentage of HT = $((ht*100/n))";
+echo "Percentage of TH = $((th*100/n))";
+winingCombination[HH]=$((hh*100/n));
+winingCombination[TT]=$((tt*100/n));
+winingCombination[HT]=$((ht*100/n));
+winingCombination[TH]=$((th*100/n));
